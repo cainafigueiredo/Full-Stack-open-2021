20df60f031b403250205a51c211edd106bd3ec41
Cainã Figueiredo <cainafigueiredo@poli.ufrj.br>
2021-09-08 20:32:41 -0300

    Exercise 2.11 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 import Filter from './components/Filter';
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => axios.get('http://localhost:3001/persons').then((response) => {
+    const persons = response.data;
+    setPersons(persons);
+  }),[]);
 
   const personsToShow = filter === '' ? persons : persons.filter(x => {
     return (x.name.toLowerCase().match(new RegExp(`^${filter.toLowerCase()}`)) !== null);
