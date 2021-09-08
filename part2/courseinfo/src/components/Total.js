a51a96d682c7f15c210a8d8930dc862d81c1ac3d
Cainã Figueiredo <cainafigueiredo@poli.ufrj.br>
2021-09-08 16:16:40 -0300

    Exercise 2.2 completed

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Total = (props) => {
-    //<Total parts={course.parts}/>
     const exercises = props.parts.map(part => part.exercises);
     const total = exercises.reduce((accumulator, curr) => accumulator+curr);
     return (
-      <p>Number of exercises {total}</p>
+      <p><b>total of {total} exercises</b></p>
     ); 
 };
 
